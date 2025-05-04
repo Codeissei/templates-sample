3261998df81b76536ede1270b538bae2411f980d
Codeissei <isseigpt@gmail.com>
2025-05-04 18:50:47 +0900

    ユーザー情報フォームにカスタムバリデータを追加し、パスワードの検証ルールを強化。英数字と特定の記号を含む必要があることを明示。

--- wtforms-sample/forms.py
+++ wtforms-sample/forms.py
@@ -5,8 +5,9 @@ from wtforms.fields import (
     EmailField, SubmitField
 )
 # 使用するvalidatorをインポート
+# 【リスト5.14】 で「ValidationError」を追加
 from wtforms.validators import (
-    DataRequired, EqualTo, Length, NumberRange, Email
+    DataRequired, EqualTo, Length, NumberRange, Email, ValidationError
 )
 # ==================================================
 # Formクラス
@@ -44,3 +45,13 @@ class UserInfoForm(Form):
     note = TextAreaField('備考: ')
     # ボタン
     submit = SubmitField('送信')
+
+    # ▼▼▼ 【リスト5.14】 ▼▼▼ 
+    # カスタムバリデータ
+    # 英数字と記号が含まれているかチェックする
+    def validate_password(self, password):
+        if not (any(c.isalpha() for c in password.data) and \
+            any(c.isdigit() for c in password.data) and \
+            any(c in '!@#$%^&*()' for c in password.data)):
+            raise ValidationError('パスワードには【英数字と記号：!@#$%^&*()】を含める必要があります')
+    # ▲▲▲ 【リスト5.14】 ▲▲▲ 
