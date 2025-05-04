3f6abee4e1a7a5b9adc196291b3432d978f6ca6c
Codeissei <isseigpt@gmail.com>
2025-05-04 17:43:55 +0900

    フィルター機能を追加し、404エラーページを実装。カスタムフィルターやフィルターを使用したテンプレートを作成。

--- templates-sample/app.py
+++ templates-sample/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort     # 【リスト4.11】
 
 # ==================================================
 # インスタンス生成
@@ -60,7 +60,6 @@ def show_jinja_class():
     hana = Hero('花咲かじいさん', 99)
     return render_template('jinja/show4.html', user = hana)
 
-# ▼▼▼【リスト3.26】▼▼▼
 # ▼▼▼▼▼ ここから【制御文】 ▼▼▼▼▼
 # 「商品」クラス
 class Item:
@@ -78,7 +77,6 @@ def show_for_list():
     item_list = [Item(1,"ダンゴ"), Item(2,"にくまん"), Item(3,"ドラ焼き")]
     return render_template('for_list.html', items = item_list)
 
-# ▲▲▲【リスト3.26】▲▲▲
 # 条件分岐
 @app.route('/if_detail/<int:id>')
 def show_if_detail(id):
@@ -91,6 +89,58 @@ def show_if_detail(id):
 def show_jinja_if(target="colorless"):
     print(target)
     return render_template('jinja/if_else.html', color=target)
+
+# フィルター：文全体
+@app.route("/filter")
+def show_filter_block():
+    word = 'pen'
+    return render_template('filter/block.html', show_word = word)
+
+# フィルター：特定の変数
+@app.route("/filter2")
+def show_filter_variable():
+    # クラスを作成
+    momo = Hero('桃太郎', 25)
+    kinta = Hero('金太郎', 35)
+    ura = Hero('浦島タロウ', 45)
+    kagu = Hero('かぐや姫', 55)
+    kasa = Hero('笠地蔵', 65)
+    # リストに詰める
+    hero_list = [momo, kinta, ura, kagu, kasa]
+    return render_template('filter/filter_list.html', heroes = hero_list)
+
+# カスタムフィルター
+@app.template_filter('truncate')
+def str_truncate(value, length=10):
+    if len(value) > length:
+        return value[:length] + "..."
+    else:
+        return value
+
+# カスタムフィルターの実行
+@app.route("/filter3")
+def show_my_filter():
+    word = '寿限無'
+    long_word = 'じゅげむじゅげむごこうのすりきれ'
+    return render_template('filter/my_filter.html', show_word1=word, show_word2=long_word)
+
+# モジュールのインポート
+from werkzeug.exceptions import NotFound
+
+# エラーハンドリング
+@app.errorhandler(NotFound)
+def show_404_page(error):
+    msg = error.description
+    print('エラー内容：',msg)
+    return render_template('errors/404.html') , 404
+
+# ▼▼▼【リスト4.12】▼▼▼
+# abort処理
+@app.route("/abort")
+def create_exception():
+    abort(404, '要求されたページやファイルが見つからない')
+# ▲▲▲【リスト4.12】▲▲▲
+
 # ==================================================
 # 実行
 # ==================================================
